0b75152bc3c32ec10afeb28b332dced9eeb6d7a9
DEVHARAM <gkfka5968@naver.com>
2019-03-12 17:44:54 +0900

    ds

--- server/db.php
+++ server/db.php
@@ -4,5 +4,6 @@
         $password="fhwm0406";
         $dbname="project";
         $connect=mysqli_connect($hostname, $username, $password, $dbname);
-	
+
+
 
--- server/login.php
+++ server/login.php
@@ -1,21 +1,26 @@
 <?php
-require_once 'db.php';
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET,POST,PUT');
+header('Access-Control-Allow-Headers: X-Requested-With,Content-Type');
+include 'db.php';
+
 try{
 	
 	//post로 받아오기
 	//$id = preg_replace('/^[a-z0-9_]{4,20}$/','',$_POST['store_id']);
 	//$pw = preg_replace('/^[a-z0-9_]{4,20}$/','',$_POST['store_password']);
-
-	$id=$_POST['store_id'];
-	$pw=$_POST['store_password'];	
+    $json_data = json_decode(file_get_contents('php://input'), TRUE);
+    $id=$json_data['store_id'];
+	$pw=$json_data['store_password'];
 	$query = "SELECT COUNT(*) AS num FROM test AS t WHERE t.id='".$id."' AND t.password='".$pw."'";
 	$stmt = mysqli_query($connect,$query);
-	$result = mysqli_fetch_assoc($stmt);	
-		
-	if($result['num']==='0'){
-		$confirm=0;
+	$result = mysqli_fetch_assoc($stmt);
+
+    $confirm=-1;
+	if($result['num']=="1"){
+		$confirm=1; //login fail
 	}else{
-		$confirm=1;
+		$confirm=0; //login success
 	}
 	 
 	$send_data=array(
