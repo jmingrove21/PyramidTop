4073fb80871f9e666c108246fc90f249a4ab3539
DEVHARAM <gkfka5968@naver.com>
2019-03-21 18:44:24 +0900

    Delete db.php

--- server/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$hostname="localhost";
-        $username="root";
-        $password="fhwm0406";
-        $dbname="project";
-        $connect=mysqli_connect($hostname, $username, $password, $dbname);
-
-
-
