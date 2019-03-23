ee035403f2366f0f0d613602ac4bc619aeb0f945
Changhee Kim <changhee94@ajou.ac.kr>
2019-03-23 20:07:51 +0900

    <app_user>

--- app_user/app/src/main/java/com/example/app_delivery/MainActivity.java
+++ app_user/app/src/main/java/com/example/app_delivery/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.app_delivery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -16,15 +17,24 @@ public class MainActivity extends AppCompatActivity {
     String[] NAMES = {"알촌","굽네치킨","홍콩반점","교촌치킨"};
     String[] PHONE = {"010-3232-2133","010-6532-4351","010-9434-2311","010-2331-1233"};
     String[] DESCRIPTIONS = {"뚝배기 밥집","구운 치킨","중국음식","정성스러운 치킨집"};
+
+    Toolbar myToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.reorder);
+
+        getSupportActionBar().setTitle("가게 목록");
+
         ListView listView = (ListView)findViewById(R.id.listView);
-
         CustomAdapter customAdapter = new CustomAdapter();
-
         listView.setAdapter(customAdapter);
     }
 
@@ -51,10 +61,12 @@ public class MainActivity extends AppCompatActivity {
 
             ImageView imageView = (ImageView)view.findViewById(R.id.imageView);
             TextView textView_name = (TextView)view.findViewById(R.id.textView_name);
+            TextView textView_phone = (TextView)view.findViewById(R.id.textView_phone);
             TextView textView_description = (TextView)view.findViewById(R.id.textView_description);
 
             imageView.setImageResource(IMAGES[i]);
             textView_name.setText(NAMES[i]);
+            textView_phone.setText(PHONE[i]);
             textView_description.setText(DESCRIPTIONS[i]);
             return view;
         }
