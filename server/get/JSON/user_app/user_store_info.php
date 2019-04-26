608ce434c66aa03ac94714f867a2cedd434db51e
DEVHARAM <gkfka5968@naver.com>
2019-04-26 19:19:19 +0900

    가게 주문 상세보기 수정& 사용자어플 가게 정보 불러오기 수정

--- server/get/JSON/user_app/user_store_info.php
+++ server/get/JSON/user_app/user_store_info.php
@@ -1,20 +1,51 @@
 <?php
     function store_info($json_data){
         include '../db.php';
-        $store_serial=$json_data['store_serial'];
+        $user_lat=$json_data['user_lat'];
+        $user_long=$json_data['user_long'];
+        $count=$json_data['count'];
+        $store_type=$json_data['store_type'];
          $query="
-         SELECT store_name, store_branch_name, store_address_jibun, store_building_name, start_time, end_time, store_restday, store_notice, store_profile_img, store_phone, s.store_main_type_name, s.store_sub_type_name, menu_type_code, menu_type_name, menu_code, menu_name
-         FROM Capstone.menu AS m
-         INNER JOIN Capstone.store AS s
-         ON m.store_serial=s.store_serial
-         WHERE m.store_serial=".$store_serial." ORDER BY menu_code;
+         SELECT store_serial,store_name, store_branch_name,store_phone, store_address_jibun, store_building_name, start_time, end_time, store_restday, store_notice, store_profile_img, store_phone, store_main_type_name, store_sub_type_name
+         FROM Capstone.store
+         WHERE store_main_type_name='".$store_type."'
+         ORDER BY ( 6371 * acos( cos( radians(".$user_lat.") ) * cos( radians( store_latitude) )
+                                    * cos( radians( store_longitude ) - radians(".$user_long.") )
+                                    + sin( radians(".$user_lat.") ) * sin( radians( store_latitude ) ) ) ) ASC LIMIT ".$count.",5
          ";
-         $stmt = mysqli_query($connect,$query);
 
-         $menu_type='';
-         $total_menu=[];
-         $menu=[];
-         while($row=mysqli_fetch_assoc($stmt)){
+         $stmt = mysqli_query($connect,$query);
+         $total_store=[];
+         while($row2=mysqli_fetch_assoc($stmt)){
+            $store=array(
+                'store_serial'=>$row2['store_serial'],
+                'store_name'=>$row2['store_name'],
+                'store_branch_name'=>$row2['store_branch_name'],
+                'store_phone'=>$row2['store_phone'],
+                'store_address_jibun'=>$row2['store_address_jibun'],
+                'store_building_name'=>$row2['store_building_name'],
+                'start_time'=>$row2['start_time'],
+                'end_time'=>$row2['end_time'],
+                'store_restday'=>$row2['store_restday'],
+                'store_notice'=>$row2['store_notice'],
+                'store_profile_img'=>$row2['store_profile_img'],
+                'store_phone'=>$row2['store_phone'],
+                'store_main_type_name'=>$row2['store_main_type_name'],
+                'store_sub_type_name'=>$row2['store_sub_type_name']
+            );
+            $query2="
+            SELECT menu_type_code, menu_type_name, menu_code, menu_name
+            FROM Capstone.menu
+            WHERE store_serial=".$row2['store_serial']."
+            ORDER BY menu_code
+            ";
+            $stmt2=mysqli_query($connect,$query2);
+
+            $menu_type='';
+            $total_menu=[];
+            $menu=[];
+            while($row=mysqli_fetch_assoc($stmt2)){
+
             if($menu_type==''||$menu_type==$row['menu_type_code']){
                 $menu_type=$row['menu_type_code'];
                 $menu_type_name=$row['menu_type_name'];
@@ -35,25 +66,13 @@
                 'menu_type_name'=>$menu_type_name,
                 'menu'=>$menu
             );
-            $total=array(
-                'store_name'=>$row['store_name'],
-                'store_branch_name'=>$row['store_branch_name'],
-                'store_address_jibun'=>$row['store_address_jibun'],
-                'store_building_name'=>$row['store_building_name'],
-                'start_time'=>$row['start_time'],
-                'end_time'=>$row['end_time'],
-                'store_restday'=>$row['store_restday'],
-                'store_notice'=>$row['store_notice'],
-                'store_profile_img'=>$row['store_profile_img'],
-                'store_phone'=>$row['store_phone'],
-                'store_main_type_name'=>$row['store_main_type_name'],
-                'store_sub_type_name'=>$row['store_sub_type_name'],
-                'menu'=>$total_menu
-            );
-         }
-         array_push($total_menu,$data);
-         $total['menu']=$total_menu;
 
-         echo json_encode($total,JSON_UNESCAPED_UNICODE);
+          }
+            array_push($total_menu,$data);
+            $store['menu']=$total_menu;
+            array_push($total_store,$store);
+         }
+
+         echo json_encode($total_store,JSON_UNESCAPED_UNICODE);
 }
 
