60663397004b39e9e6926f066b388fe892ecb406
Changhee Kim <changhee94@ajou.ac.kr>
2019-05-12 04:35:52 +0900

    <Iteration4 App_user>

--- app_user/app/src/main/java/com/example/app_user/OrderAdapter.java
+++ app_user/app/src/main/java/com/example/app_user/OrderAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -35,6 +36,7 @@ public class OrderAdapter extends ArrayAdapter<String> {
         if(convertView == null){
             inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.orderlist_layout,null);
+            convertView.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,900));
         }
         TextView text_store_name = (TextView) convertView.findViewById(R.id.store_name);
         ImageView img = (ImageView) convertView.findViewById(R.id.imageView);
@@ -43,7 +45,7 @@ public class OrderAdapter extends ArrayAdapter<String> {
         TextView text_minimum_price = (TextView) convertView.findViewById(R.id.minimum_price);
 
         text_store_name.setText(store_name[position]);
-        img.setImageResource(images[position]);
+       // img.setImageResource(images[position]);
         text_party_number.setText(party_number[position]);
         text_current_price.setText(current_price[position]);
         text_minimum_price.setText(minimum_price[position]);
--- app_user/app/src/main/java/com/example/app_user/OrderFragment.java
+++ app_user/app/src/main/java/com/example/app_user/OrderFragment.java
@@ -1,41 +1,44 @@
 package com.example.app_user;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class OrderFragment extends DialogFragment {
-
+    Bitmap bitmap;
     ListView listView;
-    String[] store_name = {"1","2","3"};
-    String[] party_number = {"a","b","c"};
-    String[] minimum_number = {"6000","8000","5000"};
-    String[] current_number = {"3000","2000","4000"};
-    int[] images = {1,2,3};
+    String[] store_name = {"1","2","3","4"};
+    String[] party_number = {"a","b","c","d"};
+    String[] minimum_number = {"6000","8000","5000","6000"};
+    String[] current_number = {"3000","2000","4000","7800"};
+    int[] images = null;
 
     @Nullable
     @Override
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_orderlist,container,false);
-
         listView = (ListView) view.findViewById(R.id.order_list);
-
-        getDialog().setTitle("현재 주문자들 현황");
 
         OrderAdapter orderAdapter = new OrderAdapter(getActivity(),store_name,party_number,minimum_number,
                 current_number,images);
+
 
         listView.setAdapter(orderAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
