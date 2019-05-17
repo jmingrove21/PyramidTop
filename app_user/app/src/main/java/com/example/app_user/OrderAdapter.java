c3435fcdf21527f1f2d29e9b201bfc67bff2351a
jmingrove21 <min_k21@naver.com>
2019-05-17 17:57:25 +0900

    주문 현황, 참여 현황 ui 개선

--- app_user/app/src/main/java/com/example/app_user/OrderAdapter.java
+++ app_user/app/src/main/java/com/example/app_user/OrderAdapter.java
@@ -27,17 +27,17 @@ public class OrderAdapter extends ArrayAdapter<String> {
             convertView.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,300));
         }
 
+        TextView text_date=(TextView)convertView.findViewById(R.id.date);
         TextView text_store_name = (TextView) convertView.findViewById(R.id.store_name);
         ImageView img = (ImageView) convertView.findViewById(R.id.imageView);
         TextView text_party_number = (TextView) convertView.findViewById(R.id.party_number);
-        TextView text_current_price = (TextView) convertView.findViewById(R.id.current_price);
-        TextView text_minimum_price = (TextView) convertView.findViewById(R.id.minimum_price);
+        TextView text_price = (TextView) convertView.findViewById(R.id.price);
 
+        text_date.setText(UtilSet.al_order.get(position).getOrder_create_date());
         text_store_name.setText(UtilSet.al_order.get(position).getStore().getStore_name());
         img.setImageBitmap(UtilSet.al_order.get(position).getStore().getStore_image());
         text_party_number.setText(String.valueOf(UtilSet.al_order.get(position).getParticipate_person()));
-        text_current_price.setText(String.valueOf(UtilSet.al_order.get(position).getTotal_order_price()));
-        text_minimum_price.setText(UtilSet.al_order.get(position).getStore().getMinimum_order_price());
+        text_price.setText((UtilSet.al_order.get(position).getTotal_order_price())+" / "+UtilSet.al_order.get(position).getStore().getMinimum_order_price());
 
         return convertView;
     }
