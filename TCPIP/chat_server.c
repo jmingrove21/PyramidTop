5eab2e7e097dfebbd1e9c9aafac1b6452eb99c9a
kimchanghee94 <kimchanghee94@naver.com>
2019-05-20 19:19:54 +0900

    <TCP_IP>

---
+++ TCPIP/chat_server.c
@@ -0,0 +1,106 @@
+#define _REENTRANT
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <pthread.h>
+#include <netinet/in.h>
+
+#define BUF_SIZE 100
+#define MAX_CLNT 256
+
+void * handle_clnt(void *arg);
+void send_msg(char * msg,int len);
+void error_handling(char * message);
+
+int clnt_cnt = 0;
+int clnt_socks[MAX_CLNT];
+pthread_mutex_t mutx;
+
+int main(int argc, char * argv[]){
+	int serv_sock,clnt_sock;
+	struct sockaddr_in serv_adr, clnt_adr;
+	socklen_t clnt_adr_sz;
+	pthread_t t_id;
+
+	if(argc!=2){
+		printf("Usage: %s <port>\n",argv[0]);
+		exit(1);
+	}
+
+	pthread_mutex_init(&mutx,NULL);
+	serv_sock = socket(PF_INET,SOCK_STREAM,0);
+	if(serv_sock==-1){
+		error_handling("socket() error");
+	}
+	memset(&serv_adr,0,sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_adr.sin_port = htons(atoi(argv[1]));
+
+	if(bind(serv_sock,(struct sockaddr*)&serv_adr,sizeof(serv_adr))==-1){
+		error_handling("bind() error");
+	}
+	if(listen(serv_sock,5)==-1){
+		error_handling("listen() error");
+	}
+
+	while(1){
+		clnt_adr_sz = sizeof(clnt_adr);
+		clnt_sock = accept(serv_sock,(struct sockaddr*)&clnt_adr,&clnt_adr_sz);
+		if(clnt_sock==-1){
+			error_handling("accept() error");
+		}
+
+		pthread_mutex_lock(&mutx);
+		clnt_socks[clnt_cnt++]=clnt_sock;
+		pthread_mutex_unlock(&mutx);
+
+		pthread_create(&t_id,NULL,handle_clnt,(void*)&clnt_sock);
+		pthread_detach(t_id);
+		printf("Connected client: IP: %s\n",inet_ntoa(clnt_adr.sin_addr));
+	}
+	close(serv_sock);
+	return 0;
+}
+
+void * handle_clnt(void *arg){
+	int clnt_sock=*((int*)arg);
+	int str_len = 0;
+	char msg[BUF_SIZE];
+
+	while((str_len = read(clnt_sock,msg,sizeof(msg)))!=0){
+		send_msg(msg,str_len);
+	}
+
+	pthread_mutex_lock(&mutx);
+	for(int i=0;i<clnt_cnt;i++){
+		if(clnt_sock==clnt_socks[i]){
+			while(i++<clnt_cnt-1){
+				clnt_socks[i]=clnt_socks[i+1];
+			}
+			break;
+		}
+	}
+	clnt_cnt--;
+	pthread_mutex_unlock(&mutx);
+	close(clnt_sock);
+	return NULL;
+}
+
+void send_msg(char * msg, int len){
+	pthread_mutex_lock(&mutx);
+	for(int i=0;i<clnt_cnt;i++){
+		write(clnt_socks[i],msg,len);
+	}
+	pthread_mutex_unlock(&mutx);
+}
+
+void error_handling(char * message){
+	fputs(message,stderr);
+	fputc('\n',stderr);
+	exit(1);
+}
---
+++ TCPIP/uecho_con_client.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 30
+
+void error_handling(char * message){
+	fputs(message, stderr);
+	fputc('\n',stderr);
+	exit(1);
+}
+
+int main(int argc,char * argv[])
+{
+	int sock;
+	char message[BUF_SIZE];
+	int str_len;
+	socklen_t adr_sz;
+
+	struct sockaddr_in serv_adr, from_adr;
+
+	if(argc!=3){
+		printf("Usage: %s <IP> <port>\n",argv[0]);
+		exit(1);
+	}
+
+	sock = socket(PF_INET, SOCK_DGRAM,0);
+	if(sock==-1){
+		error_handling("socket() error");
+	}
+
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_adr.sin_port = htons(atoi(argv[2]));
+
+	connect(sock,(struct sockaddr*)&serv_adr,sizeof(serv_adr));
+
+	while(1){
+		fputs("Insert messeage(q to quit): ",stdout);
+		fgets(message,BUF_SIZE,stdin);
+		if(!strcmp(message,"q\n")||!strcmp(message,"Q\n")){
+			break;
+		}
+
+		write(sock,message,strlen(message));
+		str_len = read(sock,message,BUF_SIZE-1);
+
+		message[str_len] = 0;
+		printf("Message from server: %s\n",message);
+	}
+	close(sock);
+	return 0;
+}
